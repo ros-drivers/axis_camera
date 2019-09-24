183dd1ce791c718ca1f7d1c58434774c1cbd3b47
Ebrahim <eshahrivar@clearpath.ai>
2019-09-24 11:56:30 -0400

    reverse pan direction

--- nodes/teleop.py
+++ nodes/teleop.py
@@ -11,7 +11,7 @@ class Teleop:
         self.enable_button = rospy.get_param('~enable_button', 1)
         self.axis_pan = rospy.get_param('~axis_pan', 0)
         self.axis_tilt = rospy.get_param('~axis_tilt', 1)
-        self.state = Axis(pan=220)
+        self.state = Axis(pan=-10)
         self.joy = None
 
         self.pub = rospy.Publisher('cmd', Axis)
@@ -25,7 +25,7 @@ class Teleop:
         while not rospy.is_shutdown():
             if self.joy != None and self.joy.buttons[self.enable_button] == 1:
                 #and (rospy.Time.now() - self.joy.header.stamp).to_sec() < 0.2:
-                self.state.pan = self.angle_wrap(self.state.pan + self.joy.axes[self.axis_pan]*5)
+                self.state.pan = self.angle_wrap(self.state.pan - self.joy.axes[self.axis_pan]*5)
                 self.state.tilt = self.state.tilt + self.joy.axes[self.axis_tilt]*5
                 if self.state.tilt > 85: self.state.tilt = 85
                 if self.state.tilt < 0: self.state.tilt = 0
